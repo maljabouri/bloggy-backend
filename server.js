be9a5d00043cacc08cbb76febfbb1a4ae7fc3472
Ma-Add Al-Jabouri <meljaboury@outlook.com>
2023-02-07 21:56:14 +0000

    Add Index Route

--- server.js
+++ server.js
@@ -2,11 +2,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+// Require Route Files
+const indexRouter = require('./routes/index')
+
 // Instantiate Express Application Object
 const app = express();
 
 // Define PORT for the API to run on
 const port = process.env.PORT || 5000;
+
+
+/**
+ * Routes
+ * 
+ * Mount imported Routers
+ */
+
+app.use(indexRouter);
 
 // Start the server and listen for requests on the given port
 app.listen(port, () => console.log(`Bloggy is listenting on ${port}`));
